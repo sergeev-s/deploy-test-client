8d7ca1f1e5e475caf8384491b94e22448a949aab
sergeev-s <semyonsergeev30@gmail.com>
2025-02-01 19:27:35 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [timeData, setTimeData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get('http://localhost:5000/time');
+      setTimeData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => { fetchData() }, []);
+
+  return (
+    <div>
+      <button onClick={fetchData}>Получить время</button>
+      {timeData && (
+        <div>
+          <p>Время: {timeData.time}</p>
+          <p>PID: {timeData.PID}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
